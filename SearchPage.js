91e34e17a83c1cfe9c3789842cecc2704c8100ea
Roman Salazar Lopez <roman@Romans-MacBook-Pro.local>
2017-06-21 12:23:45 -0700

    added a spinner view and an api call

--- SearchPage.js
+++ SearchPage.js
@@ -62,8 +62,61 @@ var styles = StyleSheet.create ({
   }
 });
 
+function urlForQueryAndPage(key, value, pageNumber) {
+  var data = {
+    country: 'uk',
+    pretty: '1',
+    encoding: 'json',
+    listing_type: 'buy',
+    action: 'search_listings',
+    page: pageNumber
+  };
+  data[key] = value;
+
+  var querystring = Object.keys(data)
+    .map(key => key + '=' + encodeURIComponent(data[key]))
+    .join('&');
+
+    return 'http://api.nestoria.co.uk/api?' + querystring;
+};
+
 class SearchPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: 'london',
+      isLoading: false
+    };
+  }
+
+  onSearchTextChanged(event) {
+    console.log('onSearchTextChanged');
+    this.setState({ searchString: event.nativeEvent.text });
+    console.log(this.state.searchString);
+    // console.log('onSearchTextChanged');
+    // this.setState({ searchString: event.nativeEvent.text });
+    // console.log(this.state.searchString);
+  }
+
+  _executeQuery(query) {
+    console.log(query);
+    this.setState({ isLoading: true });
+  }
+
+  onSearchPressed() {
+    var query = urlForQueryAndPage('place-name', this.state.searchString, 1);
+    this._executeQuery(query);
+  }
+
   render() {
+    console.log('SearchPage.render');
+
+    var spinner = this.state.isLoading ?
+      ( <ActivityIndicator
+          size='large'/> ) :
+      ( <View/>);
+
     return (
       <View style={styles.container}>
         <Text style={styles.description}>
@@ -75,8 +128,11 @@ class SearchPage extends Component {
         <View style={styles.flowRight}>
           <TextInput
             style={styles.searchInput}
+            value={this.state.searchString}
+            onChange={this.onSearchTextChanged.bind(this)}
             placeholder='Search via name or postcode'/>
               <TouchableHighlight style={styles.button}
+              onPress={this.onSearchPressed.bind(this)}
                 underlayColor='#99d9f4'>
                 <Text style={styles.buttonText}>Go</Text>
               </TouchableHighlight>
@@ -86,6 +142,7 @@ class SearchPage extends Component {
             <Text style={styles.buttonText}>Location</Text>
           </TouchableHighlight>
           <Image source={require('./Resources/house.png')} style={styles.image}/>
+          {spinner}
       </View>
     );
   }
